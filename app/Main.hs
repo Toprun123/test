757da34e06d786c58186670962f3aad44e7f42d1
Toprun123 <syed@sixzix.com>
2024-10-31 11:59:06 +0300

    Other random funcs

--- app/Main.hs
+++ app/Main.hs
@@ -22,6 +22,30 @@ prime_factors n = intercalate "" (
                     nub [format_p x p | x<-p])
     where p = factors n 2
 
+likes :: [String] -> String
+likes li
+    | (length li) == 0 = "no one likes this"
+    | (length li) == 1 = li!!0++" likes this"
+    | (length li) == 2 = li!!0++" and "++li!!1++" like this"
+    | (length li) == 3 = li!!0++", "++li!!1++" and "++li!!2++" like this"
+    | otherwise = li!!0++", "++li!!1++" and "++(show (length li-2))++" others like this"
+
+isSquare :: Integral n => n -> Bool
+isSquare n = (sqrt$fromIntegral n)==(fromIntegral.floor.sqrt$fromIntegral n)
+
+uniqueInOrder :: Eq a => [a] -> [a]
+uniqueInOrder []=[]
+uniqueInOrder [x]=[x]
+uniqueInOrder (x:xs)
+    | x==head xs=uniqueInOrder xs
+    | otherwise=x:uniqueInOrder xs
+
+getCountVowel :: String -> Int
+getCountVowel s=length[0 |x<-s,x`elem`"aeiou"]
+
+getUnique :: [Int] -> Int
+getUnique (x:y:xs)=[e |e<-x:y:xs,e/=p]!!0 where p=if(length$filter(==x)(x:y:xs))>1 then x else y
+
 multiples :: Integer -> Integer -> [Integer]
 multiples x y
     | x == 1001 = []
@@ -46,13 +70,18 @@ main = do
     hFlush stdout
     input <- getLine
     let num = read input :: Integer
-    putStrLn . init $ prime_factors $ num
+    putStrLn$likes ["Peter", "Cow", "me", "he"]
+    putStrLn.show$isSquare 25
+    putStrLn.show$uniqueInOrder "AABBCCBB"
+    putStrLn.show$getCountVowel "alpha"
+    putStrLn.show$getUnique [0, 0, 1, 0, 0]
+    putStrLn.init$prime_factors$num
     -- #1
     putStrLn (format_mul 3 5)
     -- #2
-    print (sum . filter even $ fibonacci 1 2)
+    print.sum.filter even$fibonacci 1 2
     -- #3
-    putStrLn . show . last $ factors 600851475143 2
+    putStrLn.show.last$factors 600851475143 2
     -- #4
-    putStrLn . show . maximum $ [prod | x <- (reverse [100..999]), y <- (reverse [100..999]), let prod = x * y, is_palindrome prod]
+    putStrLn.show.maximum$[prod | x<-(reverse [100..999]), y<-(reverse [100..999]), let prod = x*y, is_palindrome prod]
 
